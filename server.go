f56a065a9ea2c46fb092f052aade447cd7e34d6e
Danilo Sindric <danilosindric@gmail.com>
2025-04-22 19:56:16 +0200

    Update: add csrf protection and good default middlewares

--- server.go
+++ server.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/danilovict2/shazam-clone/handlers"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
@@ -16,6 +18,17 @@ func main() {
 
 	e := echo.New()
 	e.Static("/public", "public")
+	
+	e.Use(middleware.RequestID())
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+	e.Use(middleware.CSRFWithConfig(middleware.CSRFConfig{
+		TokenLookup:    "cookie:_csrf",
+		CookiePath:     "/",
+		CookieSecure:   true,
+		CookieHTTPOnly: true,
+		CookieSameSite: http.SameSiteStrictMode,
+	}))
 
 	e.GET("/", handlers.Home)
 
