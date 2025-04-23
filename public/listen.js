a73cdcb7ce3e55ad9b8e39e0e3e976bb3c335fec
Danilo Sindric <danilosindric@gmail.com>
2025-04-23 19:23:13 +0200

    Add: recording audio

--- public/listen.js
+++ public/listen.js
@@ -1,8 +1,37 @@
 const listenButton = document.getElementById('listen-button');
+listenButton.addEventListener('click', listen);
 
-listenButton.addEventListener('click', listen)
+const recordingDuration = 3000;
+const channels = 2;
 
 function listen() {
     listenButton.classList.toggle('pulse');
     listenButton.innerHTML = `<h2>Listening...</h2>`
+
+    navigator.mediaDevices.getUserMedia({ audio: true })
+        .then((stream) => {
+            const audioContext = new AudioContext();
+            const input = audioContext.createMediaStreamSource(stream);
+            const recorder = new Recorder(input, { numChannels: channels });
+
+            recorder.record();
+
+            setTimeout(() => {
+                recorder.stop();
+                recorder.exportWAV(sendAudio, null);
+            }, recordingDuration);
+        })
+        .catch(e => console.log(e));
+}
+
+function sendAudio(audio) {
+    const formData = new FormData();
+    formData.append('sample', audio);
+
+    fetch('/api/recognize', {
+        method: 'POST',
+        body: formData,
+    })
+    .then(r => console.log(r))
+    .catch(e => console.log(e));
 }
